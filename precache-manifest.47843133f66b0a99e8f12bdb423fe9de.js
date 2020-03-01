f405bfe203c70e53bf87becdffeae7ebde34c553
vlova <vlova@users.noreply.github.com>
2020-03-01 23:03:33 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.47843133f66b0a99e8f12bdb423fe9de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461e3e44c3fa665b5a58145312765d06",
+    "url": "/redux-sugar-prototype/index.html"
+  },
+  {
+    "revision": "561db20e1ccd111a6477",
+    "url": "/redux-sugar-prototype/static/css/main.a49c4971.chunk.css"
+  },
+  {
+    "revision": "0842a3bb868d2b548c76",
+    "url": "/redux-sugar-prototype/static/js/2.c3267a34.chunk.js"
+  },
+  {
+    "revision": "91bcc555fc9d257ab7cac84356530039",
+    "url": "/redux-sugar-prototype/static/js/2.c3267a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561db20e1ccd111a6477",
+    "url": "/redux-sugar-prototype/static/js/main.f2630211.chunk.js"
+  },
+  {
+    "revision": "1da8362aadd35f1d0082",
+    "url": "/redux-sugar-prototype/static/js/runtime-main.f2787f21.js"
+  }
+]);
